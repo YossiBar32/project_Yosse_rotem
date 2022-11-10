7f4f4442292c73d86cd6c624ff45e4fcce8a5079
YossiBar32 <116826747+YossiBar32@users.noreply.github.com>
2022-11-10 20:48:07 +0200

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+function mapDisplay() {
+  document.getElementById("showMap").style.display = "block";
+}
+
+function button1Handler() {
+  alert("Thanks for signing up");
+}
+function button2Handler() {
+  alert("The information has been deleted");
+}
